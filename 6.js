1b163c5445a6c1f30953d76814f05b2ce239753e
PiotrZmuda <piotrzmuda07@gmail.com>
2022-03-03 15:43:26 +0100

    zadania do sprawdzenia

--- 6.js
+++ 6.js
@@ -2,6 +2,27 @@
 // a) Podaj przykład i różnice między typem wartościowym, a referencyjnym? Czyli czym
 // różnią się zmienne proste od złożonych?
 
+//W skład typów prostych wchodzą : string, number, boolean, undefined,null.
+
+const a = 3;
+let b = a;
+b = b + 5;
+console.log(a)
+console.log(b)
+
+// Wartości w typach prostych znaczy tyle, że za każdym razem kiedy przypisuje się wartość do zmiennej, to jest tworzona kopia tej wartości.
+
+
+//Obiektem (czyli typem złożonym),jest każdy typ który nie jest typem wartościowym i używamy gdy mamy większą ilość wartośći jak np.obiekt, tablice, funkcje
+
+shoppingList = [
+    'banana',
+    'pizza',
+    'beer',
+    'butter',
+    'cheese'
+    ]
+
 
 // b) Stwórz obiekt person zawierający dane takie jak imie, nazwisko, wiek, posiadanie prawa
 // jazdy(true lub false) oraz czy osoba jest pijana(rowniez true false), następnie
